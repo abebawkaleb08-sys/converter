dd2ed1dbdbc17af52a51c4c41c354295fb49a34a
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2025-12-19 09:43:53 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,10 +1,33 @@
-# November converter
+class Converter:
 
 
-def eth_to_foreign(date):
-    return date + 10
+    def init(self, date, month, year):
+        
+        self.date = date
+        self.month = month
+        self.year = year
 
 
+    def convert_to_eth(self):
+        
+        # december
 
-print(eth_to_foreign(19))  # Example usage
+        self.date -= 9
+        self.month -=8
+        self.year -= 7
 
+        return f"{self.date}/{self.month}/{self.year}"
+    def convert_to_ger(self):
+        
+         self.date += 9
+         self.month +=8
+         self.year += 7
+
+         return f"{self.date}/{self.month}/{self.year}"
+
+# decmber
+
+
+converter = Converter(19, 12, 2025)
+
+print(converter.convert_to_eth())
