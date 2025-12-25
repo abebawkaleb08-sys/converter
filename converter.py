590ea24f8b3edd0d1277fe45f60d5468a89dbdef
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2025-12-25 20:25:20 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,33 +1,25 @@
 class Converter:
 
-
-    def init(self, date, month, year):
-        
+    def __init__(self, date, month, year):
         self.date = date
         self.month = month
         self.year = year
 
-
     def convert_to_eth(self):
-        
         # december
-
         self.date -= 9
-        self.month -=8
+        self.month -= 8
         self.year -= 7
-
         return f"{self.date}/{self.month}/{self.year}"
+
     def convert_to_ger(self):
-        
-         self.date += 9
-         self.month +=8
-         self.year += 7
-
-         return f"{self.date}/{self.month}/{self.year}"
-
-# decmber
+        self.date += 9
+        self.month += 8
+        self.year += 7
+        return f"{self.date}/{self.month}/{self.year}"
 
 
+# december
 converter = Converter(19, 12, 2025)
 
 print(converter.convert_to_eth())
