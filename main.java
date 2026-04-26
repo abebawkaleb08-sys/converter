9bcbcaf176ceffb14eec6cddd98c12055ce63a41
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2026-04-26 19:40:10 +0300

    Create main.javastrings

---
+++ main.java
@@ -0,0 +1,20 @@
+class Animal {
+    protected String name;
+
+    public void eat() {
+        System.out.println(name + " is eating...");
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name) {
+        this.name = name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy");
+        dog.eat();
+    }
+}
